50025c9e1d056d0c9f56f6474e596e3ad773c18f
Joakim Carlsen <joakim.carlsen@hotmail.com>
2019-04-04 12:37:26 +0200

    Base AR & camera switching

---
+++ MutantVegetableGarden/Assets/changeCameras.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class changeCameras : MonoBehaviour
+{
+    public bool arOn = false;
+
+    private GameObject arCam;
+    private GameObject digiCam;
+    private Button arBut;
+
+    public Color arOnColor;
+    public Color arOffColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        arCam = GameObject.Find("ARCamera");
+        digiCam = GameObject.Find("Main Camera");
+        arBut = GameObject.Find("AR_On_Off_Button").GetComponent<Button>();
+
+        if (arOn){
+            digiCam.SetActive(false);
+            arBut.GetComponent<Image>().color = arOnColor;
+        }
+        else{
+            arCam.SetActive(false);
+            arBut.GetComponent<Image>().color = arOffColor;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+
+    public void arToggle()
+    {
+        digiCam.SetActive(!digiCam.activeSelf);
+        arCam.SetActive(!arCam.activeSelf);
+
+        if(arBut.GetComponent<Image>().color == arOffColor)
+            arBut.GetComponent<Image>().color = arOnColor;
+        else
+            arBut.GetComponent<Image>().color = arOffColor;
+
+    }
+}
