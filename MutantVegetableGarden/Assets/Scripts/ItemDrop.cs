e7cc03baf443c694261c8248006372ed4079a8d2
Eplundare <aelozanoa@gmail.com>
2019-04-10 12:35:27 +0200

    Added Drag ingredientes (In progress)

---
+++ MutantVegetableGarden/Assets/Scripts/ItemDrop.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemDrop : MonoBehaviour, IDropHandler
+{
+    public RectTransform potPanel;
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        RectTransform potPanel = transform as RectTransform;
+        if (!RectTransformUtility.RectangleContainsScreenPoint(potPanel, Input.mousePosition))
+        {
+            Debug.Log("Dropped item");
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
